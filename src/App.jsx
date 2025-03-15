c9dbb94503c85ddde62b34754838c3218f20e2c5
Sohan Rout <sohanrout84@gmail.com>
2025-03-15 20:20:59 +0530

    added the logo to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
-  return<div>
+  return<div className="bg-gradient-to-br from-neutral-800 to-neutral-900">
     <Navigation/>
     <Hero/>
+    <About/>
   </div>;
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,42 @@ const Navbar = () => {
     <nav className="bg-black p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo on the left */}
-        <div className="text-white text-2xl font-bold">
-          <a href="/">Logo</a>
+        <div className="h-14 w-14 flex items-center space-x-2">
+          <img src="./public/logoRubleRadar.png" className="h-full" />
+          <span className="text-white font-medium">RubbleRadar</span>
         </div>
 
         {/* Navigation links in the center */}
         <ul className="flex space-x-6">
           <li>
-            <a href="/" className="text-white hover:text-gray-300">
+            <a
+              href="/"
+              className="text-amber-600 hover:text-gray-300 hover:underline underline-offset-8"
+            >
               Home
             </a>
           </li>
           <li>
-            <a href="/about" className="text-white hover:text-gray-300">
+            <a
+              href="/about"
+              className="text-amber-600 hover:text-gray-300 hover:underline underline-offset-8"
+            >
               About
             </a>
           </li>
           <li>
-            <a href="/services" className="text-white hover:text-gray-300">
-              Services
+            <a
+              href="/services"
+              className="text-amber-600 hover:text-gray-300 hover:underline underline-offset-8"
+            >
+              Features
             </a>
           </li>
           <li>
-            <a href="/contact" className="text-white hover:text-gray-300">
+            <a
+              href="/contact"
+              className="text-amber-600 hover:text-gray-300 hover:underline underline-offset-8"
+            >
               Contact
             </a>
           </li>
@@ -37,13 +50,13 @@ const Navbar = () => {
         <div className="flex space-x-4">
           <a
             href="/login"
-            className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600"
+            className="bg-amber-600 text-black px-4 py-2 rounded hover:bg-amber-700"
           >
             Login
           </a>
           <a
             href="/signup"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            className="bg-transparent border border-amber-600 text-amber-600 px-4 py-2 rounded hover:bg-amber-700 hover:text-black hover:border-amber-700"
           >
             Sign Up
           </a>
