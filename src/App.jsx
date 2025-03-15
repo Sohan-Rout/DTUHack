59e8fb67fcb7bb2807ba9d240a053ad18cc19b38
Sohan Rout <sohanrout84@gmail.com>
2025-03-15 23:16:49 +0530

    added solution section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Feature from "./components/Features";
 import Problem from "./components/Problem";
+import Solution from "./components/Solution";
 
 function App() {
   return<div className="bg-gradient-to-br from-neutral-800 to-neutral-900">
     <Navigation/>
     <Hero/>
     <Problem/>
+    <Solution/>
     <About/>
     <Feature/>
   </div>;
