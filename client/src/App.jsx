6453f80fe8a06c4ca554863ba03e732f09d7d198
Sohan Rout <sohanrout84@gmail.com>
2025-03-16 14:37:00 +0530

    updated components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Landing from "./page/Landing";
-import Login from "./page/login";
-import Signup from "./page/signup";
+import Login from "./components/login";
+import Signup from "./components/signup";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Landing />} />
-          <Route path="/page/login" element={<Login />} />
-          <Route path="/page/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </>
